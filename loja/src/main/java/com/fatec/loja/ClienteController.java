b9496ce17b1adb9daa4133e8241c48c62140cd2d
Jonathan Batista Bispo <jokz.bis@gmail.com>
2025-08-28 18:22:22 -0300

    learn: encerra aula 3 de query e login cliente

--- loja/src/main/java/com/fatec/loja/ClienteController.java
+++ loja/src/main/java/com/fatec/loja/ClienteController.java
@@ -1,8 +1,10 @@
 package com.fatec.loja;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,44 +13,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @CrossOrigin(origins = "*")
 public class ClienteController {
-  @Autowired
-  ClienteRepository bd;
+    @Autowired
+    ClienteRepository bd;
 
-  @PostMapping("/api/cliente")
-  public void gravar(@RequestBody Cliente obj){
-    bd.save(obj);
-    System.out.println("Cliente gravado com sucesso!");
-  }
-
-  @PutMapping("/api/cliente")
-  public void alterar(@RequestBody Cliente obj){
-    if(bd.existsById(obj.getCpf()))
-    bd.save(obj);
-    System.out.println("Cliente alterado com sucesso");
-  }
-  
-  @DeleteMapping("/api/cliente/{cpf}")
-  public void apagar(@PathVariable("cpf") String cpf){
-    if(bd.existsById(cpf)){
-      bd.deleteById(cpf);
-      System.out.println("Cliente removido com sucesso!");
+    @PostMapping("/api/cliente")
+    public void gravar(@RequestBody Cliente obj){
+        bd.save(obj);
+        System.out.println("Cliente cadastrado com sucesso!");
     }
-  }
 
-  @GetMapping("/api/cliente/{cpf}")
-  public Cliente carregar(@PathVariable("cpf") String cpf){
-    if(bd.existsById(cpf)){
-        return bd.findById(cpf).get();    
-    } else {
-        return new Cliente();
+    @PutMapping("/api/cliente")
+    public void alterar(@RequestBody Cliente obj){
+        if(bd.existsById(obj.getCodigo())) bd.save(obj);
+        System.out.println("Cliente alterado com sucesso!");
     }
-  }
-  @GetMapping("/api/clientes")
-  public List<Cliente> listar(){
-    return bd.findAll();
-  }
+
+    @GetMapping("/api/cliente/{codigo}")
+    public Cliente carregar(@PathVariable("codigo") int id){
+        if(bd.existsById(id)){
+            return bd.findById(id).get();
+        } else {
+            return new Cliente();
+        }
+    }
+
+    @DeleteMapping("/api/cliente/{codigo}")
+    public void apagar(@PathVariable("codigo") int id){
+        bd.deleteById(id);
+        System.out.println("cliente removido com sucesso!");
+    }
+
+    @GetMapping("/api/clientes")
+    public List<Cliente> listar(){
+        return bd.findAll();
+    }
+
+    @GetMapping("/api/cliente/inativos")
+    public List<Cliente> carregarInativos(){
+        return bd.listarInativos();
+    }
+
+    @PostMapping("/api/cliente/login")
+    public Cliente fazerLogin(@RequestBody Cliente obj){
+        Optional<Cliente> retorno = bd.fazerLogin(obj.getEmail(), obj.getSenha());
+        if(retorno.isPresent()) return retorno.get();
+        else return new Cliente();
+    }
 }
--- loja/src/main/java/com/fatec/loja/ClienteRepository.java
+++ loja/src/main/java/com/fatec/loja/ClienteRepository.java
@@ -1,9 +1,20 @@
 package com.fatec.loja;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, String>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+    @Query(value = "select * from cliente where email=?1 and senha=?2", nativeQuery = true)
+    public Optional<Cliente> fazerLogin(String email, String senha);
+
+    @Query(value = "select * from cliente where ativo=0", nativeQuery = true)
+    public List<Cliente> listarInativos();
+
 
 }
