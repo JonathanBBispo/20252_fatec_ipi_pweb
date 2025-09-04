0f95d2e7a999c5b4f7132d3fee36ab0b4714d391
Aluno <aluno@IPIRANGA.FATEC>
2025-09-04 17:27:42 -0300

    learn: encerra revisao atividade pw04

--- loja/src/main/java/com/fatec/loja/ProdutoController.java
+++ loja/src/main/java/com/fatec/loja/ProdutoController.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -50,8 +49,8 @@ public class ProdutoController {
         return bd.listarVitrine();
     }
 
-    @GetMapping("/api/produto/buscar")
-    public List<Produto> fazerBusca(@RequestParam String termo) {
-        return bd.fazerBusca(termo);
+    @GetMapping("/api/produto/busca/{termo}")
+    public List<Produto> fazerBusca(@PathVariable("termo") String termo) {
+        return bd.fazerBusca("%" + termo + "%");
     }
 }
--- loja/src/main/java/com/fatec/loja/ProdutoRepository.java
+++ loja/src/main/java/com/fatec/loja/ProdutoRepository.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-    @Query(value="select * from produto where destaque > 0 order by destaque asc", nativeQuery = true)
+    @Query(value="select * from produto where destaque > 0 order by destaque", nativeQuery = true)
     public List<Produto> listarVitrine();
 
-    @Query(value="select * from produto where keywords like %:termo%",nativeQuery = true)
-    List<Produto> fazerBusca(@Param("termo") String termo);
+    @Query(value="select * from produto where nome like ?1 or keyword like ?1",nativeQuery = true)
+    List<Produto> fazerBusca(String termo);
 }
