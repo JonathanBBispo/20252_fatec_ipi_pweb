ad01882d2c4c45cda8005dd4d6566b2b1320d9b1
Jonathan Bispo <jokz.bis@gmail.com>
2025-09-17 14:57:21 -0300

    learn: encerra revisao atividade pw05

--- loja/src/main/java/com/fatec/loja/ClienteController.java
+++ loja/src/main/java/com/fatec/loja/ClienteController.java
@@ -7,31 +7,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @CrossOrigin(origins = "*")
 public class ClienteController {
     @Autowired
-    ClienteRepository bd;
+    private ClienteRepository bd;
     @Autowired
-    LojaService service;
+    private LojaService util;
 
-    @PostMapping("/api/cliente")
+    @PostMapping("/api/cliente") 
     public void gravar(@RequestBody Cliente obj){
+        obj.setSenha(util.md5(obj.getSenha()));
         bd.save(obj);
-        System.out.println("Cliente cadastrado com sucesso!");
-        System.out.println(service.enviarEmail(obj.getEmail(), "Cadastro efetuado com sucesso!","Seu cadastro como cliente foi aprovado com sucesso! Seja bem-vindo! 🍷🗿"));
+        String email = "<b>Email de confirmação de cadastro</b><br><br>" +
+                    "seja bem vindo, "+ obj.getNome() + ", clique no link abaixo para "+
+                    "confirmar o seu cadastro.<br><br>"+
+                    "<a href='http://localhost:8080/api/cliente/efetivar/"+ util.md5(obj.getEmail()) +"'>Clique aqui</a>";
+        String retorno = util.enviaEmailHTML(obj.getEmail(), "Confirmação de novo cadastro", email);
+        System.out.println("Cliente gravado com sucesso! "+ retorno);
     }
 
     @PutMapping("/api/cliente")
     public void alterar(@RequestBody Cliente obj){
+         obj.setSenha(util.md5(obj.getSenha()));
         if(bd.existsById(obj.getCodigo())) bd.save(obj);
         System.out.println("Cliente alterado com sucesso!");
     }
@@ -39,6 +44,7 @@ public class ClienteController {
     @GetMapping("/api/cliente/{codigo}")
     public Cliente carregar(@PathVariable("codigo") int id){
         if(bd.existsById(id)){
+            System.out.println("Cliente encontrado !");
             return bd.findById(id).get();
         } else {
             return new Cliente();
@@ -46,25 +52,35 @@ public class ClienteController {
     }
 
     @DeleteMapping("/api/cliente/{codigo}")
-    public void apagar(@PathVariable("codigo") int id){
+    public void remover(@PathVariable("codigo") int id){
         bd.deleteById(id);
         System.out.println("cliente removido com sucesso!");
     }
 
-    @GetMapping("/api/clientes")
-    public List<Cliente> listar(){
-        return bd.findAll();
-    }
-
-    @GetMapping("/api/cliente/inativos")
-    public List<Cliente> carregarInativos(){
-        return bd.listarInativos();
-    }
 
     @PostMapping("/api/cliente/login")
     public Cliente fazerLogin(@RequestBody Cliente obj){
-        Optional<Cliente> retorno = bd.fazerLogin(obj.getEmail(), obj.getSenha());
-        if(retorno.isPresent()) return retorno.get();
-        else return new Cliente();
+         obj.setSenha(util.md5(obj.getSenha()));
+       Optional<Cliente> retorno 
+       =  bd.fazerLogin(obj.getEmail(), obj.getSenha());
+       if(retorno.isPresent()){
+             System.out.println("Login efetuado com sucesso!");
+            return retorno.get();
+       } else {
+            return new Cliente();
+       }
+    }
+
+    @GetMapping("/api/cliente/inativos")
+    public List<Cliente> listarInativos(){
+        return bd.listarInativos();
+    }
+
+    @PatchMapping("/api/cliente/efetivar/{codigo}")
+    public void efetivar(@PathVariable("codigo") int codigo){
+         Cliente obj = bd.findById(codigo).get();
+         obj.setAtivo(1);
+         bd.save(obj);
+         System.out.println("cliente liberado"); 
     }
 }
